30ca5b348233750c909bb4765101ed659280b580
Ibirn <kmcricciuti@gmail.com>
2020-12-02 15:13:08 -0800

    Add styling to inv. Add function to populate bag

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,33 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Bag from "./Tabs/Bag";
+import "../styles/Inventorystyles.scss";
 
 export default function Inventory(props) {
   const [bag, setBag] = useState({});
-  let bagArr = [];
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     axios.get(`/inventory`).then((response) => {
       setBag(response.data);
+      setLoaded(true);
     });
     return () => {};
   }, []);
 
-  useEffect(() => {
-    if (bag.bag) {
-      bagArr.push(bag.bag.split(""));
-    }
-  }, [bag]);
-
-  console.log();
-  return (
-    <div>
-      <p>Primary Weapon: {bag.primary_weapon || "none"}</p>
-      <p>Secondary Weapon: {bag.secondary_weapon || "none"}</p>
-      <p>Armor: {bag.armor || "armor"}</p>
-      <p>Attunement One: {bag.attunement_1 || "none"}</p>
-      <p>Attunement Two: {bag.attunement_2 || "none"}</p>
-      <p>Attunement Three: {bag.attunement_3 || "none"}</p>
-      <p>Gold: {bag.gold || 0}</p>
-      <p>Bag: {bag.bag}</p>
-    </div>
-  );
+  return <div className="inventory">{loaded && <Bag {...bag} />}</div>;
 }
--- src/components/Tabs/Bag.jsx
+++ src/components/Tabs/Bag.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-export default function Bag(props) {
+export default function props(props) {
+  let bagArr = [];
+  for (let item of props.bag.split(",")) {
+    bagArr.push(item.trim());
+  }
+  console.log(bagArr);
+
+  const bagContents = bagArr.map((item) => {
+    return <p>{item}</p>;
+  });
   return (
-    <li onClick={() => props.transition("BAG")} className="tab-item">
-      <h2>Inventory</h2>
-    </li>
+    <>
+      <div className="inv-equipped">
+        <h3>Equipped:</h3>
+        <p>Primary Weapon: {props.primary_weapon || "none"}</p>
+        <p>Secondary Weapon: {props.secondary_weapon || "none"}</p>
+        <p>Armor: {props.armor || "armor"}</p>
+        <p>Attunement One: {props.attunement_1 || "none"}</p>
+        <p>Attunement Two: {props.attunement_2 || "none"}</p>
+        <p>Attunement Three: {props.attunement_3 || "none"}</p>
+      </div>
+      <div className="inv-storage">
+        <h3>Bag:</h3>
+        {bagContents}
+        <p>Gold: {props.gold || 0}</p>
+      </div>
+    </>
   );
 }
