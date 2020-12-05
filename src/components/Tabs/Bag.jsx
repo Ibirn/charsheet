670b3244f06da5bc71c84e3a2626f2f4df7540e3
Ibirn <kmcricciuti@gmail.com>
2020-12-04 16:49:24 -0800

    Add drag and drop to inventory

--- src/components/Tabs/Bag.jsx
+++ src/components/Tabs/Bag.jsx
@@ -1,16 +1,105 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../styles/Inventorystyles.scss";
 
-export default function props(props) {
+export default function Bag(props) {
   let bagArr = [];
   for (let item of props.bag.split(",")) {
     bagArr.push(item.trim());
   }
   console.log(bagArr);
 
+  useEffect(() => {
+    let dragSrcEl = null;
+
+    function handleDragStart(e) {
+      this.style.opacity = "0.4";
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.setData("text/html", this.innerHTML);
+    }
+
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+
+      e.dataTransfer.dropEffect = "move";
+
+      return false;
+    }
+
+    function handleDragEnter(e) {
+      this.classList.add("over");
+    }
+
+    function handleDragLeave(e) {
+      this.classList.remove("over");
+    }
+
+    function handleDragStart(e) {
+      this.style.opacity = "0.4";
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.setData("text/html", this.innerHTML);
+    }
+
+    function handleDragEnd(e) {
+      this.style.opacity = "1";
+
+      items.forEach(function (item) {
+        item.classList.remove("over");
+      });
+    }
+
+    function handleDrop(e) {
+      console.log("HD: ", e, dragSrcEl, this);
+      if (e.stopPropagation) {
+        e.stopPropagation(); // stops the browser from redirecting.
+      }
+
+      if (dragSrcEl !== this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData("text/html");
+      }
+
+      return false;
+    }
+    let items = document.querySelectorAll(".equipslot");
+    items.forEach(function (item) {
+      console.log("D:", item);
+      item.addEventListener("dragstart", handleDragStart, false);
+      item.addEventListener("dragenter", handleDragEnter, false);
+      item.addEventListener("dragover", handleDragOver, false);
+      item.addEventListener("dragleave", handleDragLeave, false);
+      item.addEventListener("drop", handleDrop, false);
+      item.addEventListener("dragend", handleDragEnd, false);
+    });
+    let storage = document.querySelectorAll(".bagitem");
+    storage.forEach((item) => {
+      console.log("E:", item);
+      item.addEventListener("dragstart", handleDragStart, false);
+      item.addEventListener("dragenter", handleDragEnter, false);
+      item.addEventListener("dragover", handleDragOver, false);
+      item.addEventListener("dragleave", handleDragLeave, false);
+      item.addEventListener("drop", handleDrop, false);
+      item.addEventListener("dragend", handleDragEnd, false);
+    });
+  }, []);
+
   const bagContents = bagArr.map((item) => {
-    return <p>{item}</p>;
+    return (
+      <p className={"bagitem"} draggable={true}>
+        {item}
+      </p>
+    );
   });
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <table className="equip-table">
@@ -22,27 +111,39 @@ export default function props(props) {
         <tbody>
           <tr>
             <td>Primary Weapon</td>
-            <td>{props.primary_weapon || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.primary_weapon || "none"}
+            </td>
           </tr>
           <tr>
             <td>Secondary Weapon</td>
-            <td>{props.secondary_weapon || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.secondary_weapon || "none"}
+            </td>
           </tr>
           <tr>
             <td>Armor</td>
-            <td>{props.armor || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.armor || "none"}
+            </td>
           </tr>
           <tr>
             <td>Attunement One</td>
-            <td>{props.attunement_1 || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.attunement_1 || "none"}
+            </td>
           </tr>
           <tr>
             <td>Attunement Two</td>
-            <td>{props.attunement_2 || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.attunement_2 || "none"}
+            </td>
           </tr>
           <tr>
             <td>Attunement Three</td>
-            <td>{props.attunement_3 || "none"}</td>
+            <td className={"equipslot"} draggable={true}>
+              {props.attunement_3 || "none"}
+            </td>
           </tr>
         </tbody>
       </table>
