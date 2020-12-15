e30ebcb3a675fa86c34676d71389c83117093517
Ibirn <kmcricciuti@gmail.com>
2020-12-14 16:50:10 -0800

    Change styles

--- src/components/Inventory2.jsx
+++ src/components/Inventory2.jsx
@@ -8,62 +8,69 @@ export default function Inventory2(props) {
 
   return (
     <>
-      <table id="equip-table" draggable={false}>
-        <thead>
-          <tr>
-            <th colSpan="2">Equipment</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Primary:</td>
-            <td id="primary_weapon" className={"equip-slot"} draggable={true}>
-              {allItems.primary_weapon || "none"}
-            </td>
-          </tr>
-          <tr>
-            <td>Secondary:</td>
-            <td id="secondary_weapon" className={"equip-slot"} draggable={true}>
-              {allItems.secondary_weapon || "none"}
-            </td>
-          </tr>
-          <tr>
-            <td>Armor:</td>
-            <td id="armor" className={"equip-slot"} draggable={true}>
-              {allItems.armor || "none"}
-            </td>
-          </tr>
-          <tr>
-            <td>Attunement 1:</td>
-            <td id="attunement_1" className={"equip-slot"} draggable={true}>
-              {allItems.attunement_1 || "none"}
-            </td>
-          </tr>
-          <tr>
-            <td>Attunement 2:</td>
-            <td id="attunement_2" className={"equip-slot"} draggable={true}>
-              {allItems.attunement_2 || "none"}
-            </td>
-          </tr>
-          <tr>
-            <td>Attunement 3:</td>
-            <td id="attunement_3" className={"equip-slot"} draggable={true}>
-              {allItems.attunement_3 || "none"}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="inventory">
+        <table id="equip-table" draggable={false}>
+          <thead>
+            <tr>
+              <th colSpan="2">Equipment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Primary:</td>
+              <td id="primary_weapon" className={"equip-slot"} draggable={true}>
+                {allItems.primary_weapon || "none"}
+              </td>
+            </tr>
+            <tr>
+              <td>Secondary:</td>
+              <td
+                id="secondary_weapon"
+                className={"equip-slot"}
+                draggable={true}
+              >
+                {allItems.secondary_weapon || "none"}
+              </td>
+            </tr>
+            <tr>
+              <td>Armor:</td>
+              <td id="armor" className={"equip-slot"} draggable={true}>
+                {allItems.armor || "none"}
+              </td>
+            </tr>
+            <tr>
+              <td>Attunement 1:</td>
+              <td id="attunement_1" className={"equip-slot"} draggable={true}>
+                {allItems.attunement_1 || "none"}
+              </td>
+            </tr>
+            <tr>
+              <td>Attunement 2:</td>
+              <td id="attunement_2" className={"equip-slot"} draggable={true}>
+                {allItems.attunement_2 || "none"}
+              </td>
+            </tr>
+            <tr>
+              <td>Attunement 3:</td>
+              <td id="attunement_3" className={"equip-slot"} draggable={true}>
+                {allItems.attunement_3 || "none"}
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-      <table>
-        <thead id="bag-of-holding" className="equip-slot">
-          <tr>
-            <th>Bag of Holding</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Storage items={bag} />
-        </tbody>
-      </table>
+        <table>
+          <thead id="bag-of-holding" className="equip-slot">
+            <tr>
+              <th>Bag of Holding</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Storage items={bag} />
+          </tbody>
+        </table>
+      </div>
+
       <div>
         <input type="text" id="new-item"></input>
         <button
--- tinkering.js
+++ tinkering.js
@@ -1,1 +1,10 @@
-console.log(Math.floor(14 / 2) + -5);
+let arr = [1, 2, 3, 4, 55, 656, 856, 3, 2, 55];
+
+let unique = [];
+arr.forEach((element) => {
+  if (!unique.includes(element)) {
+    unique.push(element);
+  }
+});
+
+console.log(unique);
